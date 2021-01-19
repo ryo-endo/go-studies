d360016c1561139e9e34450c5b4145ad612fc62a
ryo-endo <to.endo.dev@gmail.com>
2021-01-19 22:19:01 +0900

    ファイル読み込みのテストケース追加。

--- io/ioutil/ioutil_test.go
+++ io/ioutil/ioutil_test.go
@@ -1,19 +1,33 @@
 package ioutil
 
 import (
+	"os"
 	"testing"
 )
+
+func checkSize(t *testing.T, path string, size int64) {
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		// %q 文字列表記(ダブルクオーテーション付き)
+		t.Fatalf("Stat %q (looking for size %d: %s", path, size, err)
+	}
+	if fileInfo.Size() != size {
+		t.Errorf("Stat %q: size %d want %d", path, fileInfo.Size(), size)
+	}
+}
 
 func TestReadFile(t *testing.T) {
 	filename := "hogehoge"
 	_, err := ReadFile(filename)
 	if err == nil {
-		t.Fatalf("Readfile #{filename}: error expected, none found")
+		t.Fatalf("Readfile %s: error expected, none found", filename)
 	}
 
 	filename = "ioutil_test.go"
-	_, err = ReadFile(filename)
+	contents, err := ReadFile(filename)
 	if err != nil {
-		t.Fatalf("ReadFile #{filename}: #{err}")
+		t.Fatalf("ReadFile %s: %v", filename, err)
 	}
+
+	checkSize(t, filename, int64(len(contents)))
 }
